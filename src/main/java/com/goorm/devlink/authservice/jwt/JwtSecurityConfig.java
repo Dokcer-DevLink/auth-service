c5fd72d72268ab8e835df761930ee19351f62718
MoonStar <moonsung0331@gmail.com>
2023-12-08 17:18:58 +0900

    feat: Spring Security에 적용하기 위한 Config 파일 작성

---
+++ src/main/java/com/goorm/devlink/authservice/jwt/JwtSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.goorm.devlink.authservice.jwt;
+
+import com.goorm.devlink.authservice.repository.UserRepository;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private TokenProvider tokenProvider;
+    private final UserRepository userRepository;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider, UserRepository userRepository) {
+        this.tokenProvider = tokenProvider;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JwtFilter customFilter = new JwtFilter(tokenProvider, userRepository);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
