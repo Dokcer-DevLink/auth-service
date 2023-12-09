59dec4973f3904f470e03530e2b5dc8dd946a328
MoonStar <moonsung0331@gmail.com>
2023-12-10 01:16:55 +0900

    refactor: JWT의 AccessToken과 RefreshToken을 함께 발급하기 위해서 수정

--- src/main/java/com/goorm/devlink/authservice/jwt/JwtFilter.java
+++ src/main/java/com/goorm/devlink/authservice/jwt/JwtFilter.java
@@ -1,7 +1,5 @@
 package com.goorm.devlink.authservice.jwt;
 
-import com.goorm.devlink.authservice.entity.User;
-import com.goorm.devlink.authservice.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
@@ -22,11 +19,9 @@ public class JwtFilter extends GenericFilterBean {
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
     private final TokenProvider tokenProvider;
-    private final UserRepository userRepository;
 
-    public JwtFilter(TokenProvider tokenProvider, UserRepository userRepository) {
+    public JwtFilter(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
-        this.userRepository = userRepository;
     }
 
     // 토큰의 인증정보를 SecurityContext에 저장하는 역할을 수행
@@ -34,7 +29,6 @@ public class JwtFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         String jwt = resolveToken(httpServletRequest);
         String requestURI = httpServletRequest.getRequestURI();
 
@@ -42,11 +36,6 @@ public class JwtFilter extends GenericFilterBean {
 
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
             Authentication authentication = tokenProvider.getAuthentication(jwt);
-
-            User user = userRepository.findOneWithAuthoritiesByEmail(authentication.getName()).get();
-            if(!user.getUserUuid().equals(httpServletRequest.getHeader("userUuid"))) {
-                httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-            }
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
             log.debug("Security Conext에 '{}' 인증 정보를 저장했습니다. {}", authentication.getName(), requestURI);
--- src/main/java/com/goorm/devlink/authservice/jwt/JwtSecurityConfig.java
+++ src/main/java/com/goorm/devlink/authservice/jwt/JwtSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.goorm.devlink.authservice.jwt;
 
-import com.goorm.devlink.authservice.repository.UserRepository;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
@@ -9,16 +8,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final TokenProvider tokenProvider;
-    private final UserRepository userRepository;
 
-    public JwtSecurityConfig(TokenProvider tokenProvider, UserRepository userRepository) {
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
-        this.userRepository = userRepository;
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        JwtFilter customFilter = new JwtFilter(tokenProvider, userRepository);
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
