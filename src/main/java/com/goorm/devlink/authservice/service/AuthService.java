1638413b786b49c365d73c8baf280602508bb095
MoonStar <moonsung0331@gmail.com>
2023-12-10 01:17:41 +0900

    refactor: 로그인 기능 비즈니스 로직 수정

---
+++ src/main/java/com/goorm/devlink/authservice/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.goorm.devlink.authservice.service;
+
+import com.goorm.devlink.authservice.dto.TokenDto;
+
+public interface AuthService {
+
+    TokenDto authorize(String email, String password);
+}
