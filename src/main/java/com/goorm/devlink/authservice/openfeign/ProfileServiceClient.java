587661e7f8d4eed4df1e0f9acc03bfa035e9c1c2
MoonStar <moonsung0331@gmail.com>
2023-12-22 15:20:05 +0900

    feat: 서비스간 통신을 하기 위한 OpenFeign 클라이언트 작성

---
+++ src/main/java/com/goorm/devlink/authservice/openfeign/ProfileServiceClient.java
@@ -0,0 +1,16 @@
+package com.goorm.devlink.authservice.openfeign;
+
+import com.goorm.devlink.authservice.openfeign.vo.reqeust.ProfileCreateReqeust;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "profile-service")
+public interface ProfileServiceClient {
+
+    @PostMapping("/api/myprofile")
+    ResponseEntity<Void> createProfile(@RequestBody ProfileCreateReqeust reqeust,
+                                       @RequestHeader("userUuid") String userUuid);
+}
