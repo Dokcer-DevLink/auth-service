8221764a41eb1ea5ff56362215c718c0697d7c54
MoonStar <moonsung0331@gmail.com>
2023-12-26 17:03:50 +0900

    refactor: Github 로그인을 위한 설정 정보를 쿠버네티스 설정 파일에 적용

--- src/main/java/com/goorm/devlink/authservice/AuthServiceApplication.java
+++ src/main/java/com/goorm/devlink/authservice/AuthServiceApplication.java
@@ -3,6 +3,7 @@ package com.goorm.devlink.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableDiscoveryClient
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableFeignClients
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
